230b16e18902384cb8c4b53b6b40e31703d56cb1
Justin Chumbley <justin.chumbley@pm.me>
2021-02-22 13:17:58 +0100

    init

---
+++ docs/index.R
@@ -0,0 +1,15 @@
+#' ---
+#' title: Examples
+#' output:
+#'    html_document:
+#'      toc: true
+#'      highlight: zenburn
+#' ---
+#' <!-- rmarkdown::render("supervised_play/nice_code.R") -->
+#' <!-- [See here.](http://brooksandrew.github.io/simpleblog/articles/render-reports-directly-from-R-scripts/) -->
+#' Set global options 
+#+ setup, warning=FALSE, message=FALSE
+# knitr::opts_chunk$set(echo = FALSE)
+
+library(here)
+![](here("docs", "dupe.jpg"))
