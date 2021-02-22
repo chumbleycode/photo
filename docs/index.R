5ac2a75cc57f05c3e7a5d73618a1a93d36f6c688
Justin Chumbley <justin.chumbley@pm.me>
2021-02-22 13:37:04 +0100

    init

--- docs/index.R
+++ docs/index.R
@@ -8,8 +8,22 @@
 #' <!-- rmarkdown::render("supervised_play/nice_code.R") -->
 #' <!-- [See here.](http://brooksandrew.github.io/simpleblog/articles/render-reports-directly-from-R-scripts/) -->
 #' Set global options 
-#+ setup, warning=FALSE, message=FALSE
+#+ setup, warning=FALSE, message=FALSE, results='asis'
 # knitr::opts_chunk$set(echo = FALSE)
 
+library(tidyverse)
 library(here)
-![](here("docs", "dupe.jpg"))
+
+plots = dir("docs", pattern = "*.jpg|.jpeg",full.names = TRUE)  
+ 
+walk(plots, ~cat("![](", file.path(.x)))
+
+for(i in plots){
+   cat("![text](",i,")")
+}
+
+plots <- list.files("docs/")
+for(i in plots){
+  filename <- file.path("docs", i)
+  cat("![text](",filename,")")
+}
